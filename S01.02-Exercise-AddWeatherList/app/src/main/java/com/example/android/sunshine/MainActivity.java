7689d40f5c0625402bcd83bc0da58e4446dc59bf
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2017-11-10 01:31:13 +0100

    Sunshine app: complete the AddWeatherList exercise

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,29 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.sunshine.data.FakeWeatherData;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // COMPLETED (1) Create a field to store the weather display TextView
+    private TextView weatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // COMPLETED (2) Use findViewById to get a reference to the weather display TextView
+        weatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        // COMPLETED (3) Create an array of Strings that contain fake weather data
+        String[] weatherData = FakeWeatherData.getFakeWeatherData();
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // COMPLETED (4) Append each String from the fake weather data array to the TextView
+        for (String data : weatherData) {
+            weatherTextView.append(data + "\n\n\n");
+        }
     }
 }
---
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/data/FakeWeatherData.java
@@ -0,0 +1,22 @@
+package com.example.android.sunshine.data;
+
+/**
+ * Fake weather data
+ * Created by Solange on 10/11/2017.
+ */
+
+public final class FakeWeatherData {
+
+    public static String[] getFakeWeatherData() {
+        return new String[] {
+                "storm",
+                "light rain",
+                "rain",
+                "snow",
+                "fog",
+                "clear",
+                "clouds",
+                "light clouds"
+        };
+    }
+}
