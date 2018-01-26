f991e8f034662c671371063cae7880ffb8e5a011
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-01-26 15:09:36 +0100

    Sunshine app: complete the smarter syncing exercise

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -15,20 +15,63 @@
  */
 package com.example.android.sunshine.sync;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
 
 
 public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+    // COMPLETED (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized = false;
 
-    //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    // COMPLETED (2) Create a synchronized public static void method called initialize
+    @SuppressLint("StaticFieldLeak")
+    synchronized public static void initialize(final Context context) {
+
+        // COMPLETED (3) Only execute this method body if sInitialized is false
+        if (! sInitialized) {
+
+            // COMPLETED (4) If the method body is executed, set sInitialized to true
+            sInitialized = true;
+
+            // COMPLETED (5) Check to see if our weather ContentProvider is empty
+            new AsyncTask<Void, Void, Void>() {
+
+                @Override
+                protected Void doInBackground(Void... voids) {
+
+                    Uri forecastUri = WeatherContract.WeatherEntry.CONTENT_URI;
+                    String[] projections = {WeatherContract.WeatherEntry._ID};
+                    ContentResolver resolver = context.getContentResolver();
+                    Cursor cursor = resolver.query(forecastUri,
+                            projections,
+                            WeatherContract.WeatherEntry.getSqlSelectForTodayOnwards(),
+                            null,
+                            null);
+
+                    // COMPLETED (6) If it is empty or we have a null Cursor, sync the weather now!
+                    if (cursor == null || cursor.getCount() == 0) {
+                        startImmediateSync(context);
+                    }
+
+                    if (cursor != null) {
+                        cursor.close();
+                    }
+
+                    return null;
+                }
+
+            }.execute();
+        }
+    }
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
